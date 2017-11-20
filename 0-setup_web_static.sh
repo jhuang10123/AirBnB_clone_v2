b91f82955f06d9f896a23792b5549cfac5e5c6bf
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-11-20 23:32:30 +0000

    correct sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
-# Set up web server
+# setting up web servers
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo "Hello world" | sudo tee /data/web_static/releases/test/index.html
-CURRENT="/data/web_static/current"
-if [ -L "$CURRENT" ]; then
-    sudo rm "$CURRENT"
-fi
-sudo ln -s /data/web_static/releases/test/ "$CURRENT"
+sudo mkdir -p /data/web_static/releases/test/
+echo "test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
-sudo sed -i "40i location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo sed -i '29 i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default 
+sudo service nginx start
