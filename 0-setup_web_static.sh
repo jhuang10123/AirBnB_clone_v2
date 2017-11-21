69572c4c6bb8a0b280f60b2e452468c708106766
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-11-21 01:12:25 +0000

    corrected task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,9 @@
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
-sudo sed -i '29 i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default 
+sudo sed -i '29 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default 
 sudo service nginx start
