00326c2d38606a1405e0401b37e3eadc6b1181e7
jhuang10123 <133@holbertonschool.com>
2017-12-14 00:53:18 +0000

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ returns Hello HBNB!"""
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns HBNB"""
+    return('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def CText(text):
+    """ returns C followed by text"""
+    return("C " + text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>', strict_slashes=False)
+def PythonText(text="is cool"):
+    """ returns Python followed by text or "is cool" as default """
+    return("Python " + text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def ifNumber(n):
+    """ displays “n is a number” only if n is an integer"""
+    return("{:d} is a number".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
