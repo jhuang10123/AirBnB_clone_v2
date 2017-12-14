40b3f8aa81ed8f5ff81a9358c9532afa9f0f7fdb
jhuang10123 <133@holbertonschool.com>
2017-12-14 00:30:26 +0000

    pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,20 +3,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """ returns Hello HBNB!"""
     return("Hello HBNB!")
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ returns HBNB"""
     return('HBNB')
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def CText(text):
     """ returns C followed by text"""
     return("C " + text.replace('_', ' '))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,20 +3,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """ returns Hello HBNB!"""
     return("Hello HBNB!")
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ returns HBNB"""
     return('HBNB')
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def CText(text):
     """ returns C followed by text"""
     return("C " + text.replace('_', ' '))
+
 
 @app.route('/python')
 @app.route('/python/<text>', strict_slashes=False)
@@ -24,5 +28,6 @@ def PythonText(text="is cool"):
     """ returns Python followed by text or "is cool" as default """
     return("Python " + text.replace('_', ' '))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
